d334616edd2af205c2f32af8c2ac2551e2313826
Aimen Sahnoun <aimensahnoun@outlook.com>
2022-10-07 22:18:41 +0300

    Move to planet scale

--- prisma/migrations/20220930165658_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[user_id]` on the table `TOTP` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "TOTP_user_id_key" ON "TOTP"("user_id");
