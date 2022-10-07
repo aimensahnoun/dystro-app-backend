38a221de2c7bdb836aa55fac907d0b75f639697c
Aimen Sahnoun <aimensahnoun@outlook.com>
2022-10-07 22:31:08 +0300

    Port

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
   app.enableCors();
-  await app.listen(3005);
+
+  await app.listen(configService.get('PORT') || 3005);
 }
 bootstrap();
