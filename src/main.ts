c28fae9856ae4fa73d84b5ec5cf3e27877856a07
Aimen Sahnoun <aimensahnoun@outlook.com>
2023-01-06 15:24:54 +0300

    feat: switch from ExpressJS to Fastify on NestJS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const configService: ConfigService = app.get(ConfigService);
 
   app.useGlobalPipes(
@@ -14,6 +21,6 @@ async function bootstrap() {
   );
   app.enableCors();
 
-  await app.listen(configService.get('PORT') || 3005);
+  await app.listen(configService.get('PORT') || 3005, '0.0.0.0');
 }
 bootstrap();
